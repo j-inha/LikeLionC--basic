64abb69cb5bd0187d74586772654ef39e3f74fec
INHA-JEONG <167923321+j-inha@users.noreply.github.com>
2025-02-27 10:25:45 +0900

    콘솔수정

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("캐릭터 \nhp:100 \natt:56.7 \n캐릭터이름 : ?? \n등급 : S");
+
+            //int hp = 100;
+            //float att = 56.7f;
+            //string c = "캐릭터";
+            //string cn = "??";
+            //string grade = "S";
+
+            //Console.WriteLine($"{c} \nhp : {hp} \natt : {att} \n캐릭터이름 : {cn} \n등급 : {grade}");
+
+            //int attack = 16755;
+            //int life = 78103;
+            //int critical = 36;
+            //int special = 1017;
+            //int subdue = 41;
+            //int swiftness = 611;
+            //int patience = 22;
+            //int skill = 39;
+
+            //Console.WriteLine($"공격력 : {attack} \n최대 생명력 : {life}\n    전투 특성    \n치명 : {critical}\n특화 : {critical}\n제압 : {subdue}\n신속 : {swiftness}\n인내 : {patience}\n숙련 : {skill}");
+
+            //float singlePrecision = 3.14f;
+            //double doublePrecision = 3.14;
+            //decimal highPrecision = 3.1415926535456m;
+
+            //Console.WriteLine(singlePrecision);
+            //Console.WriteLine(doublePrecision);
+            //Console.WriteLine(highPrecision);
+
+            //string greeting = "Hello World";
+            //string name = "Alice";
+
+            //Console.WriteLine(greeting);
+            //Console.WriteLine(name);
+            //Console.WriteLine(greeting + " " + name);
+
+            //bool isRunning = true;
+            //bool isFinished = false;
+
+            //Console.WriteLine(isRunning);
+            //Console.WriteLine(isFinished);
+
+            //Int32 number = 123;
+            //string text = "Hello";
+            //Boolean flag = true;
+
+            Console.WriteLine("  /$$$$$$ \r\n /$$__  $$\r\n| $$  \\ $$\r\n| $$  | $$\r\n|  $$$$$$/\r\n \\______/ \r\n          ");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine(
+    "  /$$$$$$    /$$$$$$ \r\n" +
+    " /$$__  $$  /$$__  $$\r\n" +
+    "| $$  \\ $$ | $$  \\ $$\r\n" +
+    "| $$  | $$ | $$  | $$\r\n" +
+    "|  $$$$$$/ |  $$$$$$/\r\n" +
+    " \\______/   \\______/ \r\n" +
+    "                     ");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.WriteLine(
+    "  /$$$$$$    /$$$$$$    /$$$$$$ \r\n" +
+    " /$$__  $$  /$$__  $$  /$$__  $$\r\n" +
+    "| $$  \\ $$ | $$  \\ $$ | $$  \\ $$\r\n" +
+    "| $$  | $$ | $$  | $$ | $$  | $$\r\n" +
+    "|  $$$$$$/ |  $$$$$$/ |  $$$$$$/\r\n" +
+    " \\______/   \\______/   \\______/ \r\n" +
+    "                                ");
+
+
+            Thread.Sleep(1000);
+            Console.WriteLine("'##::::::::'#######:::::'###::::'########::'####:'##::: ##::'######:::\r\n ##:::::::'##.... ##:::'## ##::: ##.... ##:. ##:: ###:: ##:'##... ##::\r\n ##::::::: ##:::: ##::'##:. ##:: ##:::: ##:: ##:: ####: ##: ##:::..:::\r\n ##::::::: ##:::: ##:'##:::. ##: ##:::: ##:: ##:: ## ## ##: ##::'####:\r\n ##::::::: ##:::: ##: #########: ##:::: ##:: ##:: ##. ####: ##::: ##::\r\n ##::::::: ##:::: ##: ##.... ##: ##:::: ##:: ##:: ##:. ###: ##::: ##::\r\n ########:. #######:: ##:::: ##: ########::'####: ##::. ##:. ######:::\r\n........:::.......:::..:::::..::........:::....::..::::..:::......::::");
+
+            Thread.Sleep(1000);
+            Console.Clear();
+
+            //Console.WriteLine();
+            //string n1 = Console.ReadLine();
+            //Console.WriteLine();
+            //string n2 = Console.ReadLine();
+            //Console.WriteLine();
+            //string n3 = Console.ReadLine();
+            //Console.WriteLine();
+            //string n4 = Console.ReadLine();
+            //Console.WriteLine();
+            //string n5 = Console.ReadLine();
+            //Console.WriteLine();
+            //string n6 = Console.ReadLine();
+            //Console.WriteLine();
+            //string n7 = Console.ReadLine();
+            //Console.WriteLine();
+            //string n8 = Console.ReadLine();
+            //Console.WriteLine();
+            //string n9 = Console.ReadLine();
+            //Console.WriteLine();
+            //string n10 = Console.ReadLine();
+
+
+            //Console.WriteLine($"루인 스킬 피해 : {n1}");
+            //Console.WriteLine($"카드 게이지 획득량 : {n2}");
+            //Console.WriteLine($"각성기 피해 : {n3}");
+            //Console.WriteLine($"최대 마나 : {n4}");
+            //Console.WriteLine($"전투 중 마나 회복량 : {n5}");
+            //Console.WriteLine($"비전투 중 마나 회복량 : {n6}");
+            //Console.WriteLine($"이동 속도 : {n7}");
+            //Console.WriteLine($"탈것 속도 : {n8}");
+            //Console.WriteLine($"운반 속도 : {n9}");
+            //Console.WriteLine($"스킬 재사용 대기시간 감소 : {n10}");
+
+
+
+            //string input = Console.ReadLine();
+            //string[] value = input.Split(' ');
+
+
+            //Console.WriteLine($"루인 스킬 피해 : {value[0]}");
+            //Console.WriteLine($"카드 게이지 획득량 : {value[1]}");
+            //Console.WriteLine($"각성기 피해 : {value[2]}");
+            //Console.WriteLine($"최대 마나 : {value[3]}");
+            //Console.WriteLine($"전투 중 마나 회복량 : {value[4]}");
+            //Console.WriteLine($"비전투 중 마나 회복량 : {value[5]}");
+            //Console.WriteLine($"이동 속도 : {value[6]}");
+            //Console.WriteLine($"탈것 속도 : {value[7]}");
+            //Console.WriteLine($"운반 속도 : {value[8]}");
+            //Console.WriteLine($"스킬 재사용 대기시간 감소 : {value[9]}");
+
+            //string s스킬재사용대기시간감소 = Console.ReadLine();
+            //s스킬재사용대기시간감소 = float.Parse(s스킬재사용대기시간감소);
+
+            //Console.WriteLine($"{s스킬재사용대기시간감소:F1}%");
+        }
+    }
+}
